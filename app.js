b18136a9d9abeb4fd1e57398f2c5e130e3120772
Amy Kirst <amylkirst@gmail.com>
2014-11-01 18:02:43 -0400

    Updated JS

--- app.js
+++ app.js
@@ -1,29 +1,44 @@
-// Steps
-// On page load, show question 1
-// Store current score as 0
-// Store age as variable
-// When user clicks "next", retrieve value of answer; calculate score and store in current score variable; show next question
-// Repeat until reach last question
-// Present question 5 as "Are you on high blood pressure medication?" Store answer in new variable
-// On question 6, calculate score; add 2 if answer to high blood pressure medication was yes
-// When user clicks "Next" on last question, calculate score; associate score with a percentage
-// If score is over 7.5%, show high-risk screen; if score is lower than 7.5%, show low risk screen
 
-
-
-
-
-
-
-// Set mainWrap so that if content is short, mainWrap extends 30px above bottom of window
 
 function setHeight() {
-	var desiredHeight = $(window).height() - $("header").outerHeight(true) - 60; // top and bottom padding
-	$(".mainWrap").css("min-height", desiredHeight);
-	console.log($(window).height());
-	console.log($("header").outerHeight(true));
-	console.log(desiredHeight);
+  var desiredHeight = $(window).height() - $("header").outerHeight(true) - 60; // top and bottom padding
+  $(".mainWrap").css("min-height", desiredHeight);
+  //console.log($(window).height());
+  //console.log($("header").outerHeight(true));
+  //console.log(desiredHeight);
 }
+
+function quesProcess() {
+  var position = 0;
+  var quesNum =  position + 1;
+  var totalQues = $(".question").length;
+  var score = 0;
+  // display total number of questions
+  $(".questionNumber").append(totalQues);
+
+  
+  $("button").click(function() {
+    position++;
+    quesNum = position + 1;
+  
+    // hide previous question
+    $(".question").hide();
+
+    // show next question
+    if (position < totalQues) {
+      $(".question").eq(position).show();
+      $(".questionNumber").text("Question " + quesNum + " of " + totalQues);
+    } else {
+      $(".question, .questionNumber, button").hide();   
+    } 
+    
+    // score question
+    console.log("The value of the answer was: " + $(".question").prev().val());
+
+
+    }); // end button click function
+ 
+} // end quesProcess function
 
 $(document).ready(function() {
   // set mainWrap on page load
@@ -33,7 +48,5 @@ $(document).ready(function() {
     setHeight();
   });
 
-}); 
-
-
-
+  quesProcess();
+});
