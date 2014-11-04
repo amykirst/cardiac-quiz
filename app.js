8c0068acd23d3e0c716692f1a238644517a95d7a
Amy Kirst <amylkirst@gmail.com>
2014-11-03 21:08:14 -0500

    Updated JS

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-
+// Need to provide option to restart quiz
+// Need to provide error message if option is not selected before "next" is hit
 
 function setHeight() {
   var desiredHeight = $(window).height() - $("header").outerHeight(true) - 60; // top and bottom padding
@@ -10,16 +11,18 @@ function setHeight() {
 
 function quesProcess() {
   var position = 0;
-  var quesNum =  position + 1;
   var totalQues = $(".question").length;
   var score = 0;
+  var quesNum = position + 1;
+  var age;
+
   // display total number of questions
   $(".questionNumber").append(totalQues);
 
   
   $("button").click(function() {
     position++;
-    quesNum = position + 1;
+    quesNum = position + 1; // Why do I need to declare this again here? Shouldn't it be updating the quesNum in the quesProcess function?
   
     // hide previous question
     $(".question").hide();
@@ -31,12 +34,119 @@ function quesProcess() {
     } else {
       $(".question, .questionNumber, button").hide();   
     } 
-    
-    // score question
-    console.log("The value of the answer was: " + $(".question").prev().val());
-
 
     }); // end button click function
+
+  // score question 
+  $("form :input").change(function() {
+    //console.log("The value of the answer is: " + $(this).val());
+    //console.log(position);
+    //console.log(quesNum);
+    if(quesNum == 1) {
+    	age = $(this).val();
+    	
+    	if( age >= 20 && age <= 34) {
+    		score = -7;
+    	} 
+    	else if (age >= 35 && age <= 39) {
+    		score = -3;
+        }
+        else if (age >= 40 && age <= 44) {
+          score = 0;
+        }
+        else if (age >= 45 && age <= 49) {
+          score = 3;
+        }
+        else if (age >= 50 && age <= 54) {
+          score = 6;
+        }
+        else if (age >= 55 && age <= 59) {
+          score = 8;
+        }
+        else if (age >= 60 && age <= 64) {
+          score = 10;
+        }
+        else if (age >= 65 && age <= 69) {
+          score = 12;
+        }
+        else if (age >= 70 && age <= 74) {
+          score = 14;
+        }
+        else if (age >= 75 && age <= 79) {
+          score = 16;
+        }
+    } // end question 1 if statement
+    if(quesNum == 2) {
+    	var cholesterol = $(this).val();
+    	if(cholesterol == "Less than 160") {
+          score = score + 0;
+    	}
+    	 if( (cholesterol == "160-199") && (age >= 20 && age <= 39) ) {   
+          score = score + 4;
+    	} 
+        if( (cholesterol == "160-199") && (age >= 40 && age <= 49) ) {   
+          score = score + 3;
+    	} 
+    	if( (cholesterol == "160-199") && (age >= 50 && age <= 59) ) {   
+          score = score + 2;
+    	}
+    	if( (cholesterol == "160-199") && (age >= 60 && age <= 79) ) {   
+          score = score + 1;
+    	}  
+    	if( (cholesterol == "200-239") && (age >= 20 && age <= 39) ) {   
+          score = score + 8;
+    	} 
+    	if( (cholesterol == "200-239") && (age >= 40 && age <= 49) ) {   
+          score = score + 6;
+    	} 
+    	if( (cholesterol == "200-239") && (age >= 50 && age <= 59) ) {   
+          score = score + 4;
+    	} 
+    	if( (cholesterol == "200-239") && (age >= 60 && age <= 69) ) {   
+          score = score + 2;
+    	} 
+    	if( (cholesterol == "200-239") && (age >= 70 && age <= 79) ) {   
+          score = score + 1;
+    	} 
+    	if( (cholesterol == "240-279") && (age >= 20 && age <= 39) ) {   
+          score = score + 11;
+    	} 
+    	if( (cholesterol == "240-279") && (age >= 40 && age <= 49) ) {   
+          score = score + 8;
+    	} 
+    	if( (cholesterol == "240-279") && (age >= 50 && age <= 59) ) {   
+          score = score + 5;
+    	} 
+    	if( (cholesterol == "240-279") && (age >= 60 && age <= 69) ) {   
+          score = score + 3;
+    	} 
+    	if( (cholesterol == "240-279") && (age >= 70 && age <= 79) ) {   
+          score = score + 2;
+    	} 
+    	if( (cholesterol == "Greater than 279") && (age >= 20 && age <= 39) ) {   
+          score = score + 13;
+    	} 
+    	if( (cholesterol == "Greater than 279") && (age >= 40 && age <= 49) ) {   
+          score = score + 10;
+    	} 
+    	if( (cholesterol == "Greater than 279") && (age >= 50 && age <= 59) ) {   
+          score = score + 7;
+    	} 
+    	if( (cholesterol == "Greater than 279") && (age >= 60 && age <= 69) ) {   
+          score = score + 4;
+    	} 
+    	if( (cholesterol == "Greater than 279") && (age >= 70 && age <= 79) ) {   
+          score = score + 2;
+    	} 
+    } // end question 2 if statement
+     if(quesNum == 3) {
+     
+            
+     	}
+
+     } // end question 3 if statement
+  });
+
  
 } // end quesProcess function
 
